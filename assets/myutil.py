80e7d5e938865bc358a7f9802b2c845cee91e890
kagamiqyah <403016660@qq.com>
2019-12-11 21:38:07 +0800

    2019.12.11

--- assets/myutil.py
+++ assets/myutil.py
@@ -5,24 +5,25 @@ def is_prime(n):
     if n < 2:
         return False
     
-    if n in (2, 3):
+    if n in (2,3):
         return True
-
-    for i in range(2, int(sqrt(n)) + 1):
+    
+    for i in range(2,int(sqrt(n)) + 1):
         if n % i == 0:
             return False
         
     return True
 
-def say_hi(*names, greeting='Hi', capitalized=False):
+def say_hi(*names,greeting = 'Hi', capitalized = False):
     """
     Print a string, with a greeting to everyone.
-    :param *names: tuple of names to be greeted.
-    :param greeting: 'Hello' as default.
-    :param capitalized: Whether name should be converted to capitalized before print. False as default.
-    :returns: None
+    :param *names:tuple of names to be greeted.
+    :param greeting:'Hi' as default.
+    :param capitalized:Whether name should be converted to capitalized before print. False as default.
+    :returns:None
     """
     for name in names:
         if capitalized:
             name = name.capitalize()
         print(f'{greeting}, {name}!')
+        
